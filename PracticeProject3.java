67b733b3f1168b54ffe4b5a51d9c5e6cb24d6226
UmeraNilofer <152244826+UmeraNilofer@users.noreply.github.com>
2023-11-29 12:39:56 +0530

    Add files via upload

---
+++ PracticeProject3.java
@@ -0,0 +1,27 @@
+
+public class PracticeProject3 {
+
+	public static void main(String[] args) {
+		nocall();
+		minus(22,35);
+		String res= value("Rosy!") ;
+		System.out.println("Hey, "+res);
+	}
+		
+		//no passing parameters and no return type
+		 static void nocall() {
+			System.out.println("No parameter passing and No return type");
+
+	}
+
+		//passing parameters and no return type
+		 static void minus(int x,int y) {
+			 int sub=x-y;
+			System.out.println("Subtraction of x and y:"+sub);
+		 }
+		 
+		 //passing parameters and return type
+		 static String value(String name) {
+			 return "Hello "+name;
+		 }
+}
